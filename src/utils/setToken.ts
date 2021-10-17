4e3e6a75ee22ff5c12d1a09134f784bced3f0256
whosydd <dev.youngkwok718@gmail.com>
2021-10-17 21:35:59 +0800

    reload和设置token功能基本完善，多用户以及per_page参数待定

--- src/utils/setToken.ts
+++ src/utils/setToken.ts
@@ -1,25 +1,28 @@
 import * as vscode from 'vscode'
 
 export default async (context: vscode.ExtensionContext) => {
+  // TODO: 多用户设置
+  // const multi = vscode.workspace.getConfiguration('gist-for-vscode').get('multi')
+
+  const user = 'default'
+
   const token: string | undefined = await vscode.window.showInputBox({
     title: 'Gist for VSCode',
     value: 'Please enter <Personal access tokens> from https://github.com/settings/tokens',
   })
-  // HOW:关于各种通知的语言设置
   try {
-    if (token === undefined) throw new Error('未完成设置Token！')
+    if (token === undefined) throw new Error('Setting has not been completed!')
 
-    if (token === '') throw new Error('未输入任何内容！')
+    if (token === '') throw new Error('Nothing entered!')
 
-    if (!/^ghp_/.test(token)) throw new Error('Token格式不正确！')
+    if (!/^ghp_/.test(token)) throw new Error('The format is incorrect!')
 
     // 扩展范围内保存token
-    context.globalState.update('token', token)
-    vscode.window.showInformationMessage('Token设置成功！')
+    context.globalState.update('token', { [user]: token })
+    vscode.window.showInformationMessage('Successfully!')
   } catch (error: any) {
-    vscode.window.showErrorMessage(error.message, '设置 Token', '稍后再试').then(value => {
-      if (value === '设置 Token') vscode.commands.executeCommand('gist-vscode.setToken')
-      if (value === '稍后再试') return
+    vscode.window.showErrorMessage(error.message, 'Try again', 'Later').then(value => {
+      if (value === 'Try again') vscode.commands.executeCommand('gist-vscode.setToken')
     })
   }
 }
