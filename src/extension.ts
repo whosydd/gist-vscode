adfebf695d41dc8c81ebe4960e24f1556ee51da4
whosydd <dev.youngkwok718@gmail.com>
2021-10-18 22:26:03 +0800

    添加功能：获取gist_id

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from 'vscode'
 import createGistByFile from './utils/createGistByFile'
-// import reload from './deprecate/reload'
 import delGist from './utils/delGist'
 import getAuthUserGists from './utils/getAuthUserGists'
+import getGistId from './utils/getGistId'
 import setToken from './utils/setToken'
 
 export function activate(context: vscode.ExtensionContext) {
@@ -10,8 +10,13 @@ export function activate(context: vscode.ExtensionContext) {
   let pat = vscode.commands.registerCommand('gist-vscode.setToken', () => setToken(context))
 
   // 获取验证用户的gists
-  let authUserList = vscode.commands.registerCommand('gist-vscode.getAuthUserGists', () =>
+  let getAuthUserGistsHandle = vscode.commands.registerCommand('gist-vscode.getAuthUserGists', () =>
     getAuthUserGists(context)
+  )
+
+  // 获取.gist/文件夹中文件的gist_id
+  let getGistIdHandle = vscode.commands.registerCommand('gist-vscode.getGistId', file =>
+    getGistId(file, context)
   )
 
   // 创建gist - 文件
@@ -23,10 +28,11 @@ export function activate(context: vscode.ExtensionContext) {
   )
 
   // 删除gist
-  let delGistHandle = vscode.commands.registerCommand('gist-vscode.delGist', () => {
-    delGist(context)
+  let delGistHandle = vscode.commands.registerCommand('gist-vscode.delGist', file => {
+    delGist(file, context)
   })
-  const subs = [pat, authUserList, createGistByFileHandle, delGistHandle]
+
+  const subs = [pat, getAuthUserGistsHandle, getGistIdHandle, createGistByFileHandle, delGistHandle]
   context.subscriptions.push(...subs)
 }
 
