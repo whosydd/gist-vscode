b696c65bd43cd2bc68fc30cc11e884476b94fa20
GY <dev.youngkwok718@gmail.com>
2021-11-04 03:48:33 +0800

    feat: 添加提示框

--- src/utils/rmWorkspaceState.ts
+++ src/utils/rmWorkspaceState.ts
@@ -1,9 +1,22 @@
 import * as vscode from 'vscode'
 
 export default async (context: vscode.ExtensionContext) => {
-  const data = context.workspaceState.keys()
-  data.forEach(item => context.workspaceState.update(item, undefined))
-  vscode.window.showWarningMessage('All clear!', 'Reload', 'Later').then(value => {
-    if (value === 'Reload') vscode.commands.executeCommand('workbench.action.reloadWindow')
-  })
+  vscode.window
+    .showWarningMessage(
+      'Are you serious?',
+      {
+        modal: true,
+        detail: '警告：删除缓存后，将无法通过本地文件操作gists上与之对应的文件！',
+      },
+      'OK'
+    )
+    .then(value => {
+      if (value === 'OK') {
+        const data = context.workspaceState.keys()
+        data.forEach(item => context.workspaceState.update(item, undefined))
+        vscode.window.showWarningMessage('All clear!', 'Reload', 'Later').then(value => {
+          if (value === 'Reload') vscode.commands.executeCommand('workbench.action.reloadWindow')
+        })
+      }
+    })
 }
